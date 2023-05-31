8cb5fa258d056d1ed61113cb95ce4118220fe723
Name <email@exaple.ru>
2023-05-31 08:18:40 +0300

    Создал методы для получения любимых цветов и кличек питомцев.

--- projectOnMethods/Program.cs
+++ projectOnMethods/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace projectOnMethods
 {
@@ -14,7 +15,7 @@ namespace projectOnMethods
         }
         static void GetQuestionnaire()
         {
-            (string Name, string Surname, double Age, bool HavePet, int Pets) user;
+            (string Name, string Surname, double Age, bool HavePet, int Pets, int FavColor) user;
             Console.WriteLine("Введите имя: ");
             user.Name = Console.ReadLine();
             Console.WriteLine("Введите фамилию: ");
@@ -27,10 +28,59 @@ namespace projectOnMethods
                 user.HavePet = true;
                 Console.WriteLine("Сколько у вас питомцев? ");
                 user.Pets = int.Parse(Console.ReadLine());
+                GetNicknames(user.Pets);
             }
             else
             {
                 user.HavePet = false;
+            }
+            Console.WriteLine("Сколько у вас любимых цветов?");
+            user.FavColor = int.Parse(Console.ReadLine());
+            GetFavColor(user.FavColor);
+            
+        }
+        static string[] GetNicknames(int pets)
+        {
+            string[] nicknames = new string[pets];
+            for (int i = 0; i < pets; i++)
+            {
+                nicknames[i] = Console.ReadLine();
+            }
+            return nicknames;
+        }
+        static string[] GetFavColor(int NumColor)
+        {
+            string[] color = new string[NumColor];
+            for (int i = 0; i < NumColor; i++)
+            {
+                color[i] = Console.ReadLine();
+            }
+            return color;
+        }
+        static void СheckValues(ref int age, ref int pets, ref int favColor)
+        {
+            bool CheckVal = true;
+            while (CheckVal)
+            {
+                if (age <= 0)
+                {
+                    Console.WriteLine("Вы неверно ввели возраст. Введите еще раз: ");
+                    age = int.Parse(Console.ReadLine());
+                }
+                else if (pets <= 0)
+                {
+                    Console.WriteLine("Вы неверно ввели количество питомцев. Введите еще раз: ");
+                    pets = int.Parse(Console.ReadLine());
+                }
+                else if (favColor <= 0)
+                {
+                    Console.WriteLine("Вы неверно ввели количество цветов. Введите еще раз: ");
+                    favColor = int.Parse(Console.ReadLine());
+                }
+                else 
+                {
+                    CheckVal = false;
+                }
             }
         }
     }
